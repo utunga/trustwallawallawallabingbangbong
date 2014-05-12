8dbe42dfbf94725945012772502106ad88c970d0
Miles Thompson <utunga@gmail.com>
2014-05-12 23:07:30 +1200

    mobile rendering and fix some rendering issues in ie6/7

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,3 +1,20 @@
 $(".full img").on("click", function() {
   $(this).toggleClass("zoom");
 });
+
+var nav = responsiveNav("#nav", {
+	customToggle: "#nav-toggle",
+	closeOnNavClick: true,
+	insert: "before",
+	open:function() 
+	{
+		console.log("open");
+	},
+	close:function() 
+	{
+		console.log("close");
+	}
+});
+
+
+
